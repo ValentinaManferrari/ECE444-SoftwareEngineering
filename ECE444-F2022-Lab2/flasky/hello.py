d84612172dc078f1b538e00c4cdceb6bd05e6522
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-09-26 19:44:59 -0400

    Lab2 activities completed

---
+++ ECE444-F2022-Lab2/flasky/hello.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretstring'
+bootstrap = Bootstrap(app)
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UoFT Email address?', validators=[DataRequired(), Email()])
+
+    submit = SubmitField('Submit')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        old_email = session.get('email')
+        
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+
+        if old_email is not None and old_email != form.email.data:
+
+            flash('Looks like you have changed your Email!')
+
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form = form, name = session.get('name'), email = session.get('email') )   
+
